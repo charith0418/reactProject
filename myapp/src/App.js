7e7114fe6dd5bccf36cfde09f731d4c116b97687
charith0418 <charith0418@gmail.com>
2026-04-30 17:27:15 +0530

    2nd commit

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,2 +1,14 @@
-var name = "sandeepa";
-console.log(name);
+this.a = 11;
+this.b =22;
+const obj = {
+    a : 10,
+    b :20,
+    normalfunction : function(a,b){
+        console.log(this.a+this.b);
+    },
+    arrowfunction : (a,b) =>{
+        console.log(this.a+this.b);
+    }
+}
+obj.arrowfunction();
+obj.normalfunction();
